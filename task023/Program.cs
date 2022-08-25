58ed38397c1fe5ee6621f5ff44b519053ab8f36c
m0uZ <m.fraltsov@gmail.com>
2022-08-26 01:47:04 +0300

    Решение задачи №23

---
+++ task023/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите положительное число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int stepen = 3;
+string res = string.Empty;  // Пустая строка
+while (i <= N)
+    {
+        res += Math.Pow(i, stepen) + ", ";
+        i++;
+                 
+    }
+Console.WriteLine(res);
