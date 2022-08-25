1e447e0c39918f2d85362e5c9b6d50ebbd9a68c7
m0uZ <m.fraltsov@gmail.com>
2022-08-26 02:41:38 +0300

    Решене дополнительной задачи

---
+++ optional task/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int b = a;
+double c = Math.Abs(Math.Pow(a, 2));
+
+while (b != 0)
+{
+    Console.Write("Введите число: ");
+    a = int.Parse(Console.ReadLine());
+    b += a;
+    c = c + Math.Abs(Math.Pow(a, 2));
+    if (b == 0)
+    break;
+}
+Console.WriteLine(c);
