5775fa8c6919a1174954b8f7e7c0573cc3ec0d5c
m0uZ <m.fraltsov@gmail.com>
2022-08-26 01:21:22 +0300

    Решение задачи №21

---
+++ task021/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("введите координату х1 ");
+int x1 = int.Parse (Console.ReadLine());
+Console.Write("введите координату y1 ");
+int y1 = int.Parse (Console.ReadLine());
+Console.Write("введите координату z1 ");
+int z1 = int.Parse (Console.ReadLine());
+Console.Write("введите координату x2 ");
+int x2 = int.Parse (Console.ReadLine());
+Console.Write("введите координату y2 ");
+int y2 = int.Parse (Console.ReadLine());
+Console.Write("введите координату z2 ");
+int z2 = int.Parse (Console.ReadLine());           
+
+double S = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)), 2);
+
+Console.WriteLine(S);
+
+try
+{
+Console.WriteLine(S);
+}
+catch
+{
+Console.WriteLine("Надо было вводить именно целые числа");  
+}
