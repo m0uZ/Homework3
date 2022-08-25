8529c999ad6d6e7d3b91e62c995c32da9dc0c3e5
m0uZ <m.fraltsov@gmail.com>
2022-08-25 23:45:40 +0300

    Решение задачи №19

---
+++ task019/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введе число: ");
+int number = Int32.Parse(Console.ReadLine());
+int number1 = number;
+int result = 0;
+        
+while (number1 > 0) 
+{
+    result *= 10;
+    result += number1 % 10;
+    number1 /= 10;
+}
+if (number == result)
+{
+    Console.WriteLine($"Число {number} является палиндромом.");
+}
+else
+{
+    Console.WriteLine($"Число {number} не является палиндромом.");
+}
